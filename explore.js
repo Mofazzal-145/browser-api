da620d279e94e96105d5c7e0319698460fc3ae83
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-04 00:41:51 +0600

    first file

---
+++ explore.js
@@ -0,0 +1,9 @@
+const name = 55; 
+const student = {
+    name: 'mofazzal', friend: 'mofu', job: 'business',
+    makeP: function (name) {
+        console.log(name);
+    }
+};
+student.makeP('student')
+// document.getElementById('name');
